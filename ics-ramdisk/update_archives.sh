0556126c10cf01575ff13d20cd8fd1382f06067a
Alberto96 <albertoguanti@gmail.com>
2013-06-29 23:16:47 +0200

    RAMDISK: Updated update archives script

--- ics-ramdisk/update_archives.sh
+++ ics-ramdisk/update_archives.sh
@@ -4,8 +4,24 @@ rm jb_combo.tar.gz
 rm jb_combo_b.tar.gz
 rm jb_combo_c.tar.gz
 rm jb_combo_v.tar.gz
+rm jbplus_combo.tar.gz
+rm jbplus_combo_b.tar.gz
+rm jbplus_combo_c.tar.gz
+rm jbplus_combo_v.tar.gz
+rm ics_combo.tar.gz
+rm ics_combo_b.tar.gz
+rm ics_combo_c.tar.gz
+rm ics_combo_v.tar.gz
 
+tar -pczf jbplus_combo_b.tar.gz jbplus_combo_b
+tar -pczf jbplus_combo_v.tar.gz jbplus_combo_v
+tar -pczf jbplus_combo_c.tar.gz jbplus_combo_c
+tar -pczf jbplus_combo.tar.gz jbplus_combo
 tar -pczf jb_combo_b.tar.gz jb_combo_b
 tar -pczf jb_combo_v.tar.gz jb_combo_v
 tar -pczf jb_combo_c.tar.gz jb_combo_c
 tar -pczf jb_combo.tar.gz jb_combo
+tar -pczf ics_combo_b.tar.gz ics_combo_b
+tar -pczf ics_combo_v.tar.gz ics_combo_v
+tar -pczf ics_combo_c.tar.gz ics_combo_c
+tar -pczf ics_combo.tar.gz ics_combo
