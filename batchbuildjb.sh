67636f2ffda6006f566db3e8883af64e36661a72
Alberto96 <albertoguanti@gmail.com>
2013-06-29 22:41:58 +0200

    New Kernel stuff, now Universal Kernel for JB and JBPlus

---
+++ batchbuildjb.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "GearKernel JB Batch Build..."
+
+make clean
+
+echo "Building Galaxy S Stock RAM version"
+
+./sbuildjb.sh galaxys 
+
+echo "Building Galaxy S BigMem RAM version"
+
+./sbuildjb.sh galaxys s 
+
+make clean
+
+echo "Building Vibrant Stock RAM version"
+
+./sbuildjb.sh vibrant
+
+echo "Building Vibrant BigMem RAM version"
+
+./sbuildjb.sh vibrant s
+
+make clean 
+
+echo "Building Captivate Stock RAM version"
+
+./sbuildjb.sh captivate
+
+echo "Building Captivate BigMem RAM version"
+
+./sbuildjb.sh captivate s
+
+make clean
+
+echo "Building Galaxy S B Stock RAM version"
+
+./sbuildjb.sh galaxysb 
+
+echo "Building Galaxy S B BigMem RAM version"
+
+./sbuildjb.sh galaxysb s 
+
+make clean
+
+echo "Finish"
