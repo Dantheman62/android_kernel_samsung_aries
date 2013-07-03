a84c7cf262c4ea4057edb89b8f2159ecb5519473
Alberto96 <albertoguanti@gmail.com>
2013-07-03 15:33:34 +0200

    RAMDISK: Temp. Disabled ICS Batch Update

--- ics-ramdisk/update_archives.sh
+++ ics-ramdisk/update_archives.sh
@@ -8,10 +8,7 @@ rm jbplus_combo.tar.gz
 rm jbplus_combo_b.tar.gz
 rm jbplus_combo_c.tar.gz
 rm jbplus_combo_v.tar.gz
-rm ics_combo.tar.gz
-rm ics_combo_b.tar.gz
-rm ics_combo_c.tar.gz
-rm ics_combo_v.tar.gz
+
 
 tar -pczf jbplus_combo_b.tar.gz jbplus_combo_b
 tar -pczf jbplus_combo_v.tar.gz jbplus_combo_v
@@ -21,7 +18,13 @@ tar -pczf jb_combo_b.tar.gz jb_combo_b
 tar -pczf jb_combo_v.tar.gz jb_combo_v
 tar -pczf jb_combo_c.tar.gz jb_combo_c
 tar -pczf jb_combo.tar.gz jb_combo
-tar -pczf ics_combo_b.tar.gz ics_combo_b
-tar -pczf ics_combo_v.tar.gz ics_combo_v
-tar -pczf ics_combo_c.tar.gz ics_combo_c
-tar -pczf ics_combo.tar.gz ics_combo
+
+#To Be Restored Soon
+#tar -pczf ics_combo_b.tar.gz ics_combo_b
+#tar -pczf ics_combo_v.tar.gz ics_combo_v
+#tar -pczf ics_combo_c.tar.gz ics_combo_c
+#tar -pczf ics_combo.tar.gz ics_combo
+#rm ics_combo.tar.gz
+#rm ics_combo_b.tar.gz
+#rm ics_combo_c.tar.gz
+#rm ics_combo_v.tar.gz
