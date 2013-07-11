b34012b43993954e4d4d64e881216f2d6fc293a1
Alberto96 <albertoguanti@gmail.com>
2013-07-11 16:01:27 +0200

    V Update to GearKernel 1.0.3. Changelog V
    - No more lags
    - Easier ZRAM Management
    - Lighter
    - Back to SLQB Memory Allocator
    - Removed UKSM, useless
    - Higher GPU Performance (a bit)
    - Removed lots of debug things
    
    Maybe more i don't remember, see commits for more details.

--- sbuildjbplus.sh
+++ sbuildjbplus.sh
@@ -4,31 +4,31 @@ case "$1" in
         galaxys)
             VARIANT="galaxys"
             VER=""
-	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxyS_1.0.2v2"
+	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxyS_1.0.3"
             ;;
 
         galaxysb)
             VARIANT="galaxysb"
             VER="b"
-	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxySB_1.0.2v2"
+	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxySB_1.0.3"
             ;;
 
         captivate)
             VARIANT="captivate"
             VER="c"
-	    BASE_SEMA_VER="GearKernel_JBPlus_Captivate_1.0.2v2"
+	    BASE_SEMA_VER="GearKernel_JBPlus_Captivate_1.0.3"
             ;;
 
         vibrant)
             VARIANT="vibrant"
             VER="v"
-	    BASE_SEMA_VER="GearKernel_JBPlus_Vibrant_1.0.2v2"
+	    BASE_SEMA_VER="GearKernel_JBPlus_Vibrant_1.0.3"
             ;;
 
         *)
             VARIANT="galaxys"
             VER=""
-	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxyS_1.0.2v2"
+	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxyS_1.0.3"
 esac
 
 if [ "$2" = "s" ] ; then
