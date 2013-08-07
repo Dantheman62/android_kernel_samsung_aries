a67ca6c5fd8db8fd9140be9f6f7f57d7721735db
Alberto96 <albertoguanti@gmail.com>
2013-08-07 23:24:14 +0200

    V Update to GearKernel 1.0.5 JB & JBPlus. Big Changelog V
    - Improved USB signal quality
    - Enable Deep Idle Stats by default (for some users toggle wasn't working)
    - Disabled much more debug stuff
    - Added LagFree and Wheatley CPU Governors
    - Updated to Linux Kernel 3.0.89
    - Using other vfp optimizations
    - Switched to TREE_PREEMPT_RCU for extreme performance
    - Back to SLUB Memory Allocator (seems stabler and faster)
    - Default Min CPU Freq 200000 MHz
    - LagFree Default CPU Governor
    - Enabled ARM_UNWIND
    - Disabled Frame Pointer
    
    Maybe more i don't remember :D

--- sbuildjbplus.sh
+++ sbuildjbplus.sh
@@ -4,31 +4,31 @@ case "$1" in
         galaxys)
             VARIANT="galaxys"
             VER=""
-	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxyS_1.0.4"
+	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxyS_1.0.5"
             ;;
 
         galaxysb)
             VARIANT="galaxysb"
             VER="b"
-	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxySB_1.0.4"
+	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxySB_1.0.5"
             ;;
 
         captivate)
             VARIANT="captivate"
             VER="c"
-	    BASE_SEMA_VER="GearKernel_JBPlus_Captivate_1.0.4"
+	    BASE_SEMA_VER="GearKernel_JBPlus_Captivate_1.0.5"
             ;;
 
         vibrant)
             VARIANT="vibrant"
             VER="v"
-	    BASE_SEMA_VER="GearKernel_JBPlus_Vibrant_1.0.4"
+	    BASE_SEMA_VER="GearKernel_JBPlus_Vibrant_1.0.5"
             ;;
 
         *)
             VARIANT="galaxys"
             VER=""
-	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxyS_1.0.4"
+	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxyS_1.0.5"
 esac
 
 if [ "$2" = "s" ] ; then
