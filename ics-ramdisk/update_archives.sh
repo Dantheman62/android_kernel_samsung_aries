67704c5c8c0dc6300dfd2e773d47f88b5d52460e
Alberto96 <albertoguanti@gmail.com>
2013-08-17 11:30:06 +0200

    RAMDISK: update_archives: JB3 Support

--- ics-ramdisk/update_archives.sh
+++ ics-ramdisk/update_archives.sh
@@ -4,6 +4,10 @@ rm jb_combo.tar.gz
 rm jb_combo_b.tar.gz
 rm jb_combo_c.tar.gz
 rm jb_combo_v.tar.gz
+rm jb3_combo.tar.gz
+rm jb3_combo_b.tar.gz
+rm jb3_combo_c.tar.gz
+rm jb3_combo_v.tar.gz
 rm jbplus_combo.tar.gz
 rm jbplus_combo_b.tar.gz
 rm jbplus_combo_c.tar.gz
@@ -18,8 +22,12 @@ tar -pczf jb_combo_b.tar.gz jb_combo_b
 tar -pczf jb_combo_v.tar.gz jb_combo_v
 tar -pczf jb_combo_c.tar.gz jb_combo_c
 tar -pczf jb_combo.tar.gz jb_combo
+tar -pczf jb3_combo_b.tar.gz jb3_combo_b
+tar -pczf jb3_combo_v.tar.gz jb3_combo_v
+tar -pczf jb3_combo_c.tar.gz jb3_combo_c
+tar -pczf jb3_combo.tar.gz jb3_combo
 
-#To Be Restored Soon
+# To Be Restored Soon
 #tar -pczf ics_combo_b.tar.gz ics_combo_b
 #tar -pczf ics_combo_v.tar.gz ics_combo_v
 #tar -pczf ics_combo_c.tar.gz ics_combo_c
