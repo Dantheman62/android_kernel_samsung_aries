ed501126ef90b3aebcfa09611cd735f91e2e1354
Alberto96 <albertoguanti@gmail.com>
2013-08-17 22:26:38 +0200

    V Update to GearKernel JB/JBPlus/JB3 1.0.6. Changelog V
    - JB 4.3 Support
    - Audio Drivers updated
    - ExFat filesystem support
    - Linux Kernel 3.0.91

--- sbuildjbplus.sh
+++ sbuildjbplus.sh
@@ -4,31 +4,31 @@ case "$1" in
         galaxys)
             VARIANT="galaxys"
             VER=""
-	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxyS_1.0.5"
+	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxyS_1.0.6"
             ;;
 
         galaxysb)
             VARIANT="galaxysb"
             VER="b"
-	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxySB_1.0.5"
+	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxySB_1.0.6"
             ;;
 
         captivate)
             VARIANT="captivate"
             VER="c"
-	    BASE_SEMA_VER="GearKernel_JBPlus_Captivate_1.0.5"
+	    BASE_SEMA_VER="GearKernel_JBPlus_Captivate_1.0.6"
             ;;
 
         vibrant)
             VARIANT="vibrant"
             VER="v"
-	    BASE_SEMA_VER="GearKernel_JBPlus_Vibrant_1.0.5"
+	    BASE_SEMA_VER="GearKernel_JBPlus_Vibrant_1.0.6"
             ;;
 
         *)
             VARIANT="galaxys"
             VER=""
-	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxyS_1.0.5"
+	    BASE_SEMA_VER="GearKernel_JBPlus_GalaxyS_1.0.6"
 esac
 
 if [ "$2" = "s" ] ; then
