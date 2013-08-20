093916c7bdb81b867b2e0ce88fae332412da1644
Alberto96 <albertoguanti@gmail.com>
2013-08-20 11:24:55 +0200

    RAMDISK: Update "update_archives" for new recovery method

--- ics-ramdisk/update_archives.sh
+++ ics-ramdisk/update_archives.sh
@@ -1,38 +1,25 @@
 #!/bin/sh
 
 rm jb_combo.tar.gz
-rm jb_combo_b.tar.gz
-rm jb_combo_c.tar.gz
 rm jb_combo_v.tar.gz
 rm jb3_combo.tar.gz
-rm jb3_combo_b.tar.gz
-rm jb3_combo_c.tar.gz
 rm jb3_combo_v.tar.gz
 rm jbplus_combo.tar.gz
-rm jbplus_combo_b.tar.gz
-rm jbplus_combo_c.tar.gz
 rm jbplus_combo_v.tar.gz
 
+rm ics_rec_init.tar.gz
+rm ics_rec_init_v.tar.gz
+rm ics_rec_init_c.tar.gz
+rm ics_rec_init_b.tar.gz
 
-tar -pczf jbplus_combo_b.tar.gz jbplus_combo_b
 tar -pczf jbplus_combo_v.tar.gz jbplus_combo_v
-tar -pczf jbplus_combo_c.tar.gz jbplus_combo_c
 tar -pczf jbplus_combo.tar.gz jbplus_combo
-tar -pczf jb_combo_b.tar.gz jb_combo_b
 tar -pczf jb_combo_v.tar.gz jb_combo_v
-tar -pczf jb_combo_c.tar.gz jb_combo_c
 tar -pczf jb_combo.tar.gz jb_combo
-tar -pczf jb3_combo_b.tar.gz jb3_combo_b
 tar -pczf jb3_combo_v.tar.gz jb3_combo_v
-tar -pczf jb3_combo_c.tar.gz jb3_combo_c
 tar -pczf jb3_combo.tar.gz jb3_combo
 
-# To Be Restored Soon
-#tar -pczf ics_combo_b.tar.gz ics_combo_b
-#tar -pczf ics_combo_v.tar.gz ics_combo_v
-#tar -pczf ics_combo_c.tar.gz ics_combo_c
-#tar -pczf ics_combo.tar.gz ics_combo
-#rm ics_combo.tar.gz
-#rm ics_combo_b.tar.gz
-#rm ics_combo_c.tar.gz
-#rm ics_combo_v.tar.gz
+tar -pczf ics_rec_init.tar.gz ics_rec_init
+tar -pczf ics_rec_init_v.tar.gz ics_rec_init_v
+tar -pczf ics_rec_init_c.tar.gz ics_rec_init_c
+tar -pczf ics_rec_init_b.tar.gz ics_rec_init_b
