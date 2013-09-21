9365659a5e94d226ef30d1c4e9e0572a7d13ca7d
Alberto96 <albertoguanti@gmail.com>
2013-09-21 23:10:32 +0200

    Revert "Remove useless ICS stuff, using another repo for it"
    This reverts commit 840ba9241d1d6528e992b7d01556112f2d5ce7c2.

--- ics-ramdisk/update_archives.sh
+++ ics-ramdisk/update_archives.sh
@@ -9,6 +9,11 @@ rm jb3_combo_v.tar.gz
 rm jbplus_combo.tar.gz
 rm jbplus_combo_v.tar.gz
 
+rm ics_rec_init.tar.gz
+rm ics_rec_init_v.tar.gz
+rm ics_rec_init_c.tar.gz
+rm ics_rec_init_b.tar.gz
+
 tar -pczf jbplus_combo_v.tar.gz jbplus_combo_v
 tar -pczf jbplus_combo.tar.gz jbplus_combo
 tar -pczf jb_combo_v.tar.gz jb_combo_v
@@ -17,3 +22,8 @@ tar -pczf jb_combo_m.tar.gz jb_combo_m
 tar -pczf jb_combo_mv.tar.gz jb_combo_mv
 tar -pczf jb3_combo_v.tar.gz jb3_combo_v
 tar -pczf jb3_combo.tar.gz jb3_combo
+
+tar -pczf ics_rec_init.tar.gz ics_rec_init
+tar -pczf ics_rec_init_v.tar.gz ics_rec_init_v
+tar -pczf ics_rec_init_c.tar.gz ics_rec_init_c
+tar -pczf ics_rec_init_b.tar.gz ics_rec_init_b
