840ba9241d1d6528e992b7d01556112f2d5ce7c2
Alberto96 <albertoguanti@gmail.com>
2013-09-21 23:07:58 +0200

    Remove useless ICS stuff, using another repo for it

--- ics-ramdisk/update_archives.sh
+++ ics-ramdisk/update_archives.sh
@@ -9,11 +9,6 @@ rm jb3_combo_v.tar.gz
 rm jbplus_combo.tar.gz
 rm jbplus_combo_v.tar.gz
 
-rm ics_rec_init.tar.gz
-rm ics_rec_init_v.tar.gz
-rm ics_rec_init_c.tar.gz
-rm ics_rec_init_b.tar.gz
-
 tar -pczf jbplus_combo_v.tar.gz jbplus_combo_v
 tar -pczf jbplus_combo.tar.gz jbplus_combo
 tar -pczf jb_combo_v.tar.gz jb_combo_v
@@ -22,8 +17,3 @@ tar -pczf jb_combo_m.tar.gz jb_combo_m
 tar -pczf jb_combo_mv.tar.gz jb_combo_mv
 tar -pczf jb3_combo_v.tar.gz jb3_combo_v
 tar -pczf jb3_combo.tar.gz jb3_combo
-
-tar -pczf ics_rec_init.tar.gz ics_rec_init
-tar -pczf ics_rec_init_v.tar.gz ics_rec_init_v
-tar -pczf ics_rec_init_c.tar.gz ics_rec_init_c
-tar -pczf ics_rec_init_b.tar.gz ics_rec_init_b
