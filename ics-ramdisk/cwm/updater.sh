f6a1f6dd51d30af8ed8c9ff3d76ae8ee70d41a4d
Alberto96 <albertoguanti@gmail.com>
2013-09-21 23:07:32 +0200

    RAMDISK: Finally! Fix EFS Backup/Restore Workaround

--- ics-ramdisk/cwm/updater.sh
+++ ics-ramdisk/cwm/updater.sh
@@ -1,13 +1,15 @@
 #!/sbin/busybox sh
-#
-# create a backup of efs
-#
+# GearKernel Script
+# Create a backup of efs partition
+# by Alberto96
 
-mount -t yaffs2 /dev/block/mtdblock4 /efs
-mount -t ext4 /dev/lvpool/system /system
-rm -rf /system/GearKernel/efs
-mkdir -p /system/GearKernel
-mkdir -p /system/GearKernel/efs
-cp -Rf /efs/* /system/GearKernel/efs/
-umount /efs
-umount /system
+busybox umount /system
+busybox mkdir /efs
+busybox mount -t yaffs2 /dev/block/mtdblock4 /efs
+busybox mount -t ext4 /dev/lvpool/system /system
+busybox rm -rf /system/GearKernel/efs
+busybox mkdir -p /system/GearKernel
+busybox mkdir -p /system/GearKernel/efs
+busybox cp -Rf /efs/* /system/GearKernel/efs/
+busybox umount /efs
+busybox umount /system
