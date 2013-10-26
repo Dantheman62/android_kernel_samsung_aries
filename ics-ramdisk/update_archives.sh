b12094c945e85a5a000f7f967f7f0ad841f1bc79
Alberto96 <albertoguanti@gmail.com>
2013-10-26 16:37:31 +0200

    Updated "Updater" Script for Universal Method

--- ics-ramdisk/update_archives.sh
+++ ics-ramdisk/update_archives.sh
@@ -1,27 +1,15 @@
 #!/bin/sh
 
-rm jb_combo.tar.gz
-rm jb_combo_m.tar.gz
-rm jb_combo_mv.tar.gz
-rm jb_combo_v.tar.gz
-rm jb3_combo.tar.gz
-rm jb3_combo_v.tar.gz
-rm jbplus_combo.tar.gz
-rm jbplus_combo_v.tar.gz
+rm gear_combo.tar.gz
+rm gear_combo_v.tar.gz
 
 rm ics_rec_init.tar.gz
 rm ics_rec_init_v.tar.gz
 rm ics_rec_init_c.tar.gz
 rm ics_rec_init_b.tar.gz
 
-tar -pczf jbplus_combo_v.tar.gz jbplus_combo_v
-tar -pczf jbplus_combo.tar.gz jbplus_combo
-tar -pczf jb_combo_v.tar.gz jb_combo_v
-tar -pczf jb_combo.tar.gz jb_combo
-tar -pczf jb_combo_m.tar.gz jb_combo_m
-tar -pczf jb_combo_mv.tar.gz jb_combo_mv
-tar -pczf jb3_combo_v.tar.gz jb3_combo_v
-tar -pczf jb3_combo.tar.gz jb3_combo
+tar -pczf gear_combo.tar.gz gear_combo
+tar -pczf gear_combo_v.tar.gz gear_combo_v
 
 tar -pczf ics_rec_init.tar.gz ics_rec_init
 tar -pczf ics_rec_init_v.tar.gz ics_rec_init_v
